c6022f11b92307ace75dbfe00706eb45e03d5ed5
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-21 22:59:02 +0200

    git route

--- src/auth/callback/route.ts
+++ src/auth/callback/route.ts
@@ -1,7 +1,19 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { type NextRequest } from "next/server";
-import { cookies } from "next/headers";
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { type NextRequest, NextResponse } from 'next/server'
 
-export default async function callback(req: NextRequest) {
-  const supabase = createRouteHandlerClient({ cookies });
+// esto es una opción de Next.js, para evitar que cachee de forma
+// estática la ruta, y que siempre se ejecute en el servidor
+export const dynamic = 'force-dynamic'
+
+export async function GET (request: NextRequest) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+
+  if (code !== null) {
+    const supabase = createRouteHandlerClient({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  return NextResponse.redirect(requestUrl.origin)
 }
