0780ebdfb45ebd58565bd94ccad159f73dc9cacc
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-21 16:48:56 +0200

    github login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthButton } from "./components/auth-button";
 
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: posts } = await supabase.from("posts").select("*");
 
-export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola Twitter
+      <AuthButton />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </main>
   );
 }
